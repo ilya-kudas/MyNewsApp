1f0973f80b9e82f670d53f9bcefccc26daed2783
ilyak <ilya.kudas@globallogic.com>
2017-03-15 21:40:27 +0200

    1

---
+++ app/src/main/java/com/example/ilyak/mynewsapp/NewsMessage.java
@@ -0,0 +1,14 @@
+package com.example.ilyak.mynewsapp;
+
+import android.media.Image;
+
+/**
+ * Created by ilyak on 15.03.2017.
+ */
+
+public class NewsMessage {
+    public String Title;
+    public String Digest;
+    public String Text;
+    public Image Image;
+}
